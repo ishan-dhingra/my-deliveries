81fb6ef7574b1e8aa452724d58ea6cf072f9db6d
Ishan Dhingra <Ishan.Dhingra@snapdeal.com>
2017-09-03 18:42:54 +0530

    Toaster showShort Green

--- app/src/main/java/com/anythingintellect/mydeliveries/util/Toaster.java
+++ app/src/main/java/com/anythingintellect/mydeliveries/util/Toaster.java
@@ -23,6 +23,8 @@ class Toaster {
     }
 
     public void showShort(String msg) {
-
+        Toast toast = Toast.makeText(context, msg, Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.CENTER, 0 , 0);
+        toast.show();
     }
 }
