c8e179778f4ee7dec2629dba98c8cc9ff19a21eb
Ishan Dhingra <Ishan.Dhingra@snapdeal.com>
2017-09-03 18:39:59 +0530

    Toaster showLong Green

--- app/src/main/java/com/anythingintellect/mydeliveries/util/Toaster.java
+++ app/src/main/java/com/anythingintellect/mydeliveries/util/Toaster.java
@@ -1,6 +1,8 @@
 package com.anythingintellect.mydeliveries.util;
 
 import android.content.Context;
+import android.view.Gravity;
+import android.widget.Toast;
 
 /**
  * Created by ishan.dhingra on 03/09/17.
@@ -8,11 +10,15 @@ import android.content.Context;
 
 class Toaster {
 
-    public Toaster(Context applicationContext) {
+    private final Context context;
 
+    public Toaster(Context context) {
+        this.context = context;
     }
 
     public void showLong(String msg) {
-
+        Toast toast = Toast.makeText(context, msg, Toast.LENGTH_LONG);
+        toast.setGravity(Gravity.CENTER, 0, 0);
+        toast.show();
     }
 }
