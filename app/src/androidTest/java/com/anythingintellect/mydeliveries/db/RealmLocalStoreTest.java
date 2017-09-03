f7750ff6225887ad7bb0d04d5f2b0c6f1288e743
Ishan Dhingra <Ishan.Dhingra@snapdeal.com>
2017-09-03 16:14:40 +0530

    RealmStore getDeliveries Should return sorted output Red

--- app/src/androidTest/java/com/anythingintellect/mydeliveries/db/RealmLocalStoreTest.java
+++ app/src/androidTest/java/com/anythingintellect/mydeliveries/db/RealmLocalStoreTest.java
@@ -4,6 +4,7 @@ import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
 import com.anythingintellect.mydeliveries.model.Delivery;
+import com.anythingintellect.mydeliveries.model.Location;
 import com.anythingintellect.mydeliveries.util.MockData;
 
 import org.junit.Before;
@@ -87,6 +88,7 @@ public class RealmLocalStoreTest {
                 });
 
                 RealmResults<Delivery> deliveriesResults = localStore.getDeliveries();
+
                 assertNotEquals(null, deliveriesResults);
                 deliveriesResults.load();
                 assertEquals(deliveries.size(), deliveriesResults.size());
@@ -96,6 +98,42 @@ public class RealmLocalStoreTest {
     }
 
     // Should return deliveries in sorted by id order
+    @Test
+    public void testGetDeliveries_ShouldReturnDeliveriesSortedByIdAscending() {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                final List<Delivery> deliveries = MockData.getNDeliveries(5);
+                Delivery firstDeliver = new Delivery();
+                firstDeliver.setDescription("Test");
+                firstDeliver.setId(109L);
+                firstDeliver.setLocation(new Location());
+                deliveries.add(0, firstDeliver);
+                realm.executeTransaction(new Realm.Transaction() {
+                    @Override
+                    public void execute(Realm realm) {
+                        realm.copyToRealmOrUpdate(deliveries);
+                    }
+                });
+
+                RealmResults<Delivery> realmResults = localStore.getDeliveries();
+                realmResults.load();
+
+                assertEquals(deliveries.size(), realmResults.size());
+                Delivery last = realmResults.get(0);
+                boolean isSorted = true;
+                for (int i = 1; i < realmResults.size(); i++) {
+                    Delivery current = realmResults.get(i);
+                    if (last.getId() > current.getId()) {
+                        isSorted = false;
+                        break;
+                    }
+                    last = current;
+                }
+                assertEquals(true, isSorted);
+            }
+        });
+    }
 
 
     // Should close realm on close call
