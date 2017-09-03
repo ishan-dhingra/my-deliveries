0f51948895ce4210c047cf9d1c2a5f395ac9a3cf
Ishan Dhingra <Ishan.Dhingra@snapdeal.com>
2017-09-03 18:37:44 +0530

    Toaster showLong Red

---
+++ app/src/main/java/com/anythingintellect/mydeliveries/util/Toaster.java
@@ -0,0 +1,18 @@
+package com.anythingintellect.mydeliveries.util;
+
+import android.content.Context;
+
+/**
+ * Created by ishan.dhingra on 03/09/17.
+ */
+
+class Toaster {
+
+    public Toaster(Context applicationContext) {
+
+    }
+
+    public void showLong(String msg) {
+
+    }
+}
---
+++ app/src/test/java/com/anythingintellect/mydeliveries/util/ToasterTest.java
@@ -0,0 +1,44 @@
+package com.anythingintellect.mydeliveries.util;
+
+import android.widget.Toast;
+
+import com.anythingintellect.mydeliveries.BaseTest;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.RuntimeEnvironment;
+import org.robolectric.annotation.Config;
+import org.robolectric.shadows.ShadowToast;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by ishan.dhingra on 03/09/17.
+ */
+
+@RunWith(RobolectricTestRunner.class)
+@Config(manifest = Config.NONE)
+public class ToasterTest extends BaseTest {
+
+    private Toaster toaster;
+
+    @Before
+    public void setup() {
+        toaster = new Toaster(RuntimeEnvironment.application.getApplicationContext());
+    }
+
+    // showLong
+    // Should show long toast
+    @Test
+    public void testShowLong_ShouldShowLongToast() {
+        String msg = "test";
+        toaster.showLong(msg);
+        assertEquals(msg, ShadowToast.getTextOfLatestToast());
+        assertEquals(Toast.LENGTH_LONG, ShadowToast.getLatestToast().getDuration());
+    }
+
+    // showShort
+    // Should show short toast
+}
